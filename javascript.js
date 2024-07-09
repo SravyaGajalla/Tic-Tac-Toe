84782a4f401246e7704f33954c96223753abfb3f
SravyaGajalla <sravyagajalla1502@gmail.com>
2024-07-09 23:10:16 +0530

    first commit

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ let resetBTN = document.querySelector("#reset");
 let newGameBtn = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".msg-container")
 let msg = document.querySelector("#msg");
-
+// let msg1
 let turnO = true;
 
 const winPattrens = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]];
