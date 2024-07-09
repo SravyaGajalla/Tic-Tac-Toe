a88a65e6845f27079aaebe6839fb54133fdd76cb
SravyaGajalla <sravyagajalla1502@gmail.com>
2024-07-09 23:08:45 +0530

    "hey"

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ let msg = document.querySelector("#msg");
 
 let turnO = true;
 
-// const winPattrens = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]];
+const winPattrens = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]];
 
 const resetGame = ()=>{
     turnO =true;
